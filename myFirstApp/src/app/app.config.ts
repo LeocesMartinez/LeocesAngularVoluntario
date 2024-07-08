0aa85903ac37e96f20ef5a1b55e9d5ea82b6242a
Leonel <leonel.martinez@globalmvm.com>
2024-07-08 14:40:05 -0500

    Se modifica el componente base del módulo y se crean dos componentes llamados padre e hijo para empezar a manipularlos y adquirir habilidades de manejo del funcionamiento de angular

--- myFirstApp/src/app/app.config.ts
+++ myFirstApp/src/app/app.config.ts
@@ -1,7 +1,13 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, NgModule } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+
+import { PadreComponent } from './padre/padre.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { HijoComponent } from './hijo/hijo.component';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
